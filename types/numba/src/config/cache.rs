642ef28b4c4a962896fb7cd808a37e75fd20a695
craole-cc <134658831+craole-cc@users.noreply.github.com>
2024-10-24 16:29:36 -0500

    added cache

---
+++ types/numba/src/config/cache.rs
@@ -0,0 +1,16 @@
+use dashmap::DashMap;
+use lru::LruCache;
+use once_cell::sync::Lazy;
+use std::{num::NonZeroUsize, sync::Mutex};
+
+const CACHE_SIZE: usize = 10_000;
+pub static CACHE: Lazy<Cache> = Lazy::new(Cache::new);
+
+#[derive(Debug, Default)]
+pub struct Cache {}
+
+impl Cache {
+	pub fn new() -> Self {
+		Self::default()
+	}
+}
---
+++ types/numba/src/core/decimal/cache.rs
@@ -0,0 +1,66 @@
+use dashmap::DashMap;
+use lru::LruCache;
+use once_cell::sync::Lazy;
+use std::{num::NonZeroUsize, sync::Mutex};
+
+const CACHE_SIZE: usize = 10_000;
+pub static CACHE: Lazy<Cache> = Lazy::new(Cache::new);
+
+#[derive(Debug)]
+pub struct Cache {
+	for_rust_decimal: DashMap<String, rust_decimal::Decimal>,
+
+	#[cfg(feature = "big-decimal")]
+	for_big_decimal: Mutex<LruCache<String, bigdecimal::BigDecimal>>,
+}
+
+impl Default for Cache {
+	fn default() -> Self {
+		Self {
+			for_rust_decimal: DashMap::with_capacity(CACHE_SIZE),
+
+			#[cfg(feature = "big-decimal")]
+			for_big_decimal: Mutex::new(LruCache::new(
+				NonZeroUsize::new(CACHE_SIZE).unwrap(),
+			)),
+		}
+	}
+}
+
+impl Cache {
+	pub fn new() -> Self {
+		Self::default()
+	}
+
+	pub fn get_rust_decimal(
+		&self,
+		key: &str,
+	) -> Option<rust_decimal::Decimal> {
+		self.for_rust_decimal.get(key).map(|v| *v)
+	}
+
+	#[cfg(feature = "big-decimal")]
+	pub fn get_big_decimal(
+		&self,
+		key: &str,
+	) -> Option<bigdecimal::BigDecimal> {
+		self.for_big_decimal.lock().unwrap().get(key).cloned()
+	}
+
+	pub fn insert_rust_decimal(
+		&self,
+		key: String,
+		value: rust_decimal::Decimal,
+	) {
+		self.for_rust_decimal.insert(key, value);
+	}
+
+	#[cfg(feature = "big-decimal")]
+	pub fn insert_big_decimal(
+		&self,
+		key: String,
+		value: bigdecimal::BigDecimal,
+	) {
+		self.for_big_decimal.lock().unwrap().put(key, value);
+	}
+}
